4429cb730f39f682078c51497e813535397fe20d
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-01-15 21:29:05 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('user_name')->unique();
-            $table->string('avatar')->nullable();;
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
